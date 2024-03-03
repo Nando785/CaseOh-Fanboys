73ffb2990bc35614600aa7fba6aac8ddd4304a6f
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2024-03-02 20:46:59 -0800

    Added main page layout
    Created the basic layout for the main page

--- visibly/src/App.js
+++ visibly/src/App.js
@@ -3,30 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      
-      <head> // future title card
-        <style>
-          
-        </style>
-      </head>
-      <body>
+    <html>
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        
+        <h1 class="title-header">
+          Audio Project
+        </h1>
+        
+        <div class="nav-bar">
+          <a href="#" class="nav-element"> Option 1 </a>
+          <a href="#" class="nav-element"> Option 2 </a>
+        </div>
 
-      </body>
+        <div class="content">
+
+        </div>
+      </div>
+    </html>
+  );
+}
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About us</h2>
+      <p>This is the about us page</p>
     </div>
   );
 }
